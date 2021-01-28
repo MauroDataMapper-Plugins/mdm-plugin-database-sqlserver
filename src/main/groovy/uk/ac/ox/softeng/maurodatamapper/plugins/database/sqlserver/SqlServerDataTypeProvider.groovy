5dc4247370410aa3be525623207a4bcd4f936ac3
Ollie Freeman <oliver.freeman@bdi.ox.ac.uk>
2021-01-28 15:52:24 +0000

    Update LICENSE and NOTICE with the copyright to include NHSD

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/sqlserver/SqlServerDataTypeProvider.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/sqlserver/SqlServerDataTypeProvider.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 University of Oxford
+ * Copyright 2020 University of Oxford and Health and Social Care Information Centre, also known as NHS Digital
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
  */
 package uk.ac.ox.softeng.maurodatamapper.plugins.database.sqlserver
 
-import uk.ac.ox.softeng.maurodatamapper.datamodel.item.datatype.DataTypeService
+
 import uk.ac.ox.softeng.maurodatamapper.datamodel.item.datatype.PrimitiveType
 import uk.ac.ox.softeng.maurodatamapper.datamodel.provider.DefaultDataTypeProvider
 import uk.ac.ox.softeng.maurodatamapper.datamodel.rest.transport.DefaultDataType
--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/sqlserver/SqlServerDatabaseDataModelImporterProviderServiceParameters.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/sqlserver/SqlServerDatabaseDataModelImporterProviderServiceParameters.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 University of Oxford
+ * Copyright 2020 University of Oxford and Health and Social Care Information Centre, also known as NHS Digital
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,9 +22,8 @@ import uk.ac.ox.softeng.maurodatamapper.core.provider.importer.parameter.config.
 import uk.ac.ox.softeng.maurodatamapper.plugins.database.DatabaseDataModelImporterProviderServiceParameters
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource
-import net.sourceforge.jtds.jdbcx.JtdsDataSource
-
 import groovy.util.logging.Slf4j
+import net.sourceforge.jtds.jdbcx.JtdsDataSource
 
 @Slf4j
 // @CompileStatic
