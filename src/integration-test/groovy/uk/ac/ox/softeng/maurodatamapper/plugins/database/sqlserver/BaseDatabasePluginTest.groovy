739f4bc403b504dc7ff5ca11db6f877e180fa943
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2022-11-25 13:28:02 +0000

    Update database plugin version
    - Model is now saved by importer so don't save in tests

--- src/integration-test/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/sqlserver/BaseDatabasePluginTest.groovy
+++ src/integration-test/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/sqlserver/BaseDatabasePluginTest.groovy
@@ -101,11 +101,6 @@ abstract class BaseDatabasePluginTest<P extends DatabaseDataModelImporterProvide
         importDataModelAndRetrieveFromDatabase(params)
     }
 
-
-    DataModel saveDomain(DataModel domain) {
-        dataModelService.saveModelWithContent(domain)
-    }
-
     protected P createDatabaseImportParameters(String host, int port) {
         P params = createDatabaseImportParameters()
         params.setDatabaseHost(host)
@@ -175,7 +170,6 @@ abstract class BaseDatabasePluginTest<P extends DatabaseDataModelImporterProvide
             log.info('Validating imported model')
             if (dataModelService.validate(importedModel)) {
                 log.info('Saving valid imported model')
-                saveDomain(importedModel)
             } else {
                 GormUtils.outputDomainErrors(getMessageSource(), importedModel)
                 fail('Domain is invalid')
@@ -215,8 +209,7 @@ abstract class BaseDatabasePluginTest<P extends DatabaseDataModelImporterProvide
 
             if (validate) {
                 importedModels.each {domain ->
-                    if (domain.validate()) saveDomain(domain)
-                    else {
+                    if (!domain.validate()) {
                         GormUtils.outputDomainErrors(getMessageSource(), domain)
                         fail('Domain is invalid')
                     }
